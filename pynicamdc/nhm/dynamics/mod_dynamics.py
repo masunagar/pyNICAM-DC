bugfix: coriolis sign error at pole

pynicamdc/nhm/dynamics/mod_dynamics.py
@@ -465,7 +465,26 @@ class Dyn:
                 )
 
                 if nl == 1:
-                    print("started 2nd lstep loop, nl = ", nl)
+                    print("in 2nd lstep loop, nl = ", nl)
+                    ic= 6
+                    jc= 5
+                    kc= 41
+                    lc= 0
+                    with open (std.fname_log, 'a') as log_file:
+                        print(f"DIAG[{ic}, {jc}, {kc}, {lc}, I_vx]",     DIAG[ic, jc, kc, lc, I_vx], file=log_file)
+                        print(f"DIAG[{ic}, {jc}, {kc}, {lc}, I_vy]",     DIAG[ic, jc, kc, lc, I_vy], file=log_file)
+                        print(f"DIAG[{ic}, {jc}, {kc}, {lc}, I_vz]",     DIAG[ic, jc, kc, lc, I_vz], file=log_file)
+                        print(f"DIAG[{ic}, {jc}, {kc}, {lc}, I_w]",      DIAG[ic, jc, kc, lc, I_w], file=log_file)
+                        print(f"DIAG[{ic}, {jc}, {kc}, {lc}, I_tem]",    DIAG[ic, jc, kc, lc, I_tem], file=log_file)
+                        print(f"DIAG[{ic}, {jc}, {kc}, {lc}, I_pre]",    DIAG[ic, jc, kc, lc, I_pre], file=log_file)
+                        print(f"PROG[{ic}, {jc}, {kc}, {lc}, I_RHOG]",   PROG[ic, jc, kc, lc, I_RHOG], file=log_file)
+                        print(f"PROG[{ic}, {jc}, {kc}, {lc}, I_RHOGVX]", PROG[ic, jc, kc, lc, I_RHOGVX], file=log_file)
+                        print(f"PROG[{ic}, {jc}, {kc}, {lc}, I_RHOGVY]", PROG[ic, jc, kc, lc, I_RHOGVY], file=log_file)
+                        print(f"PROG[{ic}, {jc}, {kc}, {lc}, I_RHOGVZ]", PROG[ic, jc, kc, lc, I_RHOGVZ], file=log_file)
+                        print(f"PROG[{ic}, {jc}, {kc}, {lc}, I_RHOGW]",  PROG[ic, jc, kc, lc, I_RHOGW], file=log_file)
+                        print(f"PROG[{ic}, {jc}, {kc}, {lc}, I_RHOGE]",  PROG[ic, jc, kc, lc, I_RHOGE], file=log_file)
+
+
                     #prc.prc_mpistop(std.io_l, std.fname_log)
 
                 #call BNDCND_all
@@ -574,33 +593,34 @@ class Dyn:
 
                     # Task1b
                     #print("Task1b done")
-                    bndc.BNDCND_all(
+                    bndc.BNDCND_all_pl(
                         adm.ADM_gall_pl, 
-                        1, 
                         adm.ADM_kdall, 
                         adm.ADM_lall_pl,
-                        rho_pl [:, np.newaxis, :, :],                # [INOUT] view with additional dimension may stay after the BNDCND_all call. Squeeze it back later explicitly.
-                        DIAG_pl[:, np.newaxis, :, :, I_vx],          # [INOUT]
-                        DIAG_pl[:, np.newaxis, :, :, I_vy],          # [INOUT]
-                        DIAG_pl[:, np.newaxis, :, :, I_vz],          # [INOUT]
-                        DIAG_pl[:, np.newaxis, :, :, I_w],           # [INOUT]
-                        ein_pl [:, np.newaxis, :, :],                # [INOUT]
-                        DIAG_pl[:, np.newaxis, :, :, I_tem],         # [INOUT]%
-                        DIAG_pl[:, np.newaxis, :, :, I_pre],         # [INOUT]
-                        PROG_pl[:, np.newaxis, :, :, I_RHOG],        # [INOUT]
-                        PROG_pl[:, np.newaxis, :, :, I_RHOGVX],      # [INOUT]
-                        PROG_pl[:, np.newaxis, :, :, I_RHOGVY],      # [INOUT]
-                        PROG_pl[:, np.newaxis, :, :, I_RHOGVZ],      # [INOUT]
-                        PROG_pl[:, np.newaxis, :, :, I_RHOGW],       # [INOUT]
-                        PROG_pl[:, np.newaxis, :, :, I_RHOGE],       # [INOUT]
-                        vmtr.VMTR_GSGAM2_pl   [:, np.newaxis, :, :],    # [IN] view with additional dimension is temporaly, i.e., shape does not change after BNDCND_all call
-                        vmtr.VMTR_PHI_pl      [:, np.newaxis, :, :],    # [IN]
-                        vmtr.VMTR_C2Wfact_pl  [:, np.newaxis, :, :, :], # [IN]
-                        vmtr.VMTR_C2WfactGz_pl[:, np.newaxis, :, :, :], # [IN]
+                        rho_pl [:, :, :],                # [INOUT] view with additional dimension may stay after the BNDCND_all call. Squeeze it back later explicitly.
+                        DIAG_pl[:, :, :, I_vx],          # [INOUT]
+                        DIAG_pl[:, :, :, I_vy],          # [INOUT]
+                        DIAG_pl[:, :, :, I_vz],          # [INOUT]
+                        DIAG_pl[:, :, :, I_w],           # [INOUT]
+                        ein_pl [:, :, :],                # [INOUT]
+                        DIAG_pl[:, :, :, I_tem],         # [INOUT]%
+                        DIAG_pl[:, :, :, I_pre],         # [INOUT]
+                        PROG_pl[:, :, :, I_RHOG],        # [INOUT]
+                        PROG_pl[:, :, :, I_RHOGVX],      # [INOUT]
+                        PROG_pl[:, :, :, I_RHOGVY],      # [INOUT]
+                        PROG_pl[:, :, :, I_RHOGVZ],      # [INOUT]
+                        PROG_pl[:, :, :, I_RHOGW],       # [INOUT]
+                        PROG_pl[:, :, :, I_RHOGE],       # [INOUT]
+                        vmtr.VMTR_GSGAM2_pl   [:, :, :],    # [IN] view with additional dimension is temporaly, i.e., shape does not change after BNDCND_all call
+                        vmtr.VMTR_PHI_pl      [:, :, :],    # [IN]
+                        vmtr.VMTR_C2Wfact_pl  [:, :, :, :], # [IN]
+                        vmtr.VMTR_C2WfactGz_pl[:, :, :, :], # [IN]
                         cnst,
                         rdtype,
                     )
 
+                    # changed to using func_pl, because np.newaxis sometimes cause issues when using func
+                    # probably giving a dummy dimension for poles in the entire code would be better
 
                     # with open(std.fname_log, 'a') as log_file:
                     #     print("after BNDCND_all fore POLE", file=log_file)
@@ -631,32 +651,56 @@ class Dyn:
                     # ein_pl = ein_pl.squeeze(axis=1)
 
                     # Task2
-                    #print("Task2b done but not tested yet")
+                    # #print("Task2b done but not tested yet")
+                    # th_pl = tdyn.THRMDYN_th(
+                    #     adm.ADM_gall_pl, 
+                    #     1, 
+                    #     adm.ADM_kdall, 
+                    #     adm.ADM_lall_pl, 
+                    #     DIAG_pl[:, np.newaxis, :, :, I_tem], 
+                    #     DIAG_pl[:, np.newaxis, :, :, I_pre],
+                    #     cnst,
+                    # )
+                    # th_pl = np.squeeze(th_pl, axis=1) # removing dummy dimension
+
+                    # This function should work without newaxis
                     th_pl = tdyn.THRMDYN_th(
                         adm.ADM_gall_pl, 
                         1, 
                         adm.ADM_kdall, 
                         adm.ADM_lall_pl, 
-                        DIAG_pl[:, np.newaxis, :, :, I_tem], 
-                        DIAG_pl[:, np.newaxis, :, :, I_pre],
+                        DIAG_pl[:, :, :, I_tem], 
+                        DIAG_pl[:, :, :, I_pre],
                         cnst,
                     )
-                    th_pl = np.squeeze(th_pl, axis=1) # removing dummy dimension
+                    
                     
                     # Task3
                     #print("Task3b done but not tested yet")
+                    # eth_pl = tdyn.THRMDYN_eth(
+                    #     adm.ADM_gall_pl, 
+                    #     1, 
+                    #     adm.ADM_kdall, 
+                    #     adm.ADM_lall_pl, 
+                    #     ein_pl [:, np.newaxis, :, :],  
+                    #     DIAG_pl[:, np.newaxis, :, :, I_pre],
+                    #     rho_pl [:, np.newaxis, :, :], 
+                    #     cnst,
+                    # )
+                    # eth_pl = np.squeeze(eth_pl, axis=1) # removing dummy dimension
+
+                    # This function should work without newaxis
                     eth_pl = tdyn.THRMDYN_eth(
                         adm.ADM_gall_pl, 
                         1, 
                         adm.ADM_kdall, 
                         adm.ADM_lall_pl, 
-                        ein_pl [:, np.newaxis, :, :],  
-                        DIAG_pl[:, np.newaxis, :, :, I_pre],
-                        rho_pl [:, np.newaxis, :, :], 
+                        ein_pl [:, :, :],  
+                        DIAG_pl[:, :, :, I_pre],
+                        rho_pl [:, :, :], 
                         cnst,
                     )
-                    eth_pl = np.squeeze(eth_pl, axis=1) # removing dummy dimension
-
+                    
                     # perturbations ( pre, rho with metrics )
                     pregd_pl[:, :, :] = (DIAG_pl[:, :, :, I_pre] - pre_bs_pl) * vmtr.VMTR_GSGAM2_pl
                     rhogd_pl[:, :, :] = (rho_pl - rho_bs_pl) * vmtr.VMTR_GSGAM2_pl
@@ -699,10 +743,10 @@ class Dyn:
                         PROG  [:,:,:,:,I_RHOGVY], PROG_pl  [:,:,:,I_RHOGVY], # [IN]
                         PROG  [:,:,:,:,I_RHOGVZ], PROG_pl  [:,:,:,I_RHOGVZ], # [IN]
                         PROG  [:,:,:,:,I_RHOGW],  PROG_pl  [:,:,:,I_RHOGW],  # [IN]
-                        g_TEND[:,:,:,:,I_RHOGVX], g_TEND_pl[:,:,:,I_RHOGVX], # [OUT]
-                        g_TEND[:,:,:,:,I_RHOGVY], g_TEND_pl[:,:,:,I_RHOGVY], # [OUT]
-                        g_TEND[:,:,:,:,I_RHOGVZ], g_TEND_pl[:,:,:,I_RHOGVZ], # [OUT]
-                        g_TEND[:,:,:,:,I_RHOGW],  g_TEND_pl[:,:,:,I_RHOGW],  # [OUT]
+                        g_TEND[:,:,:,:,I_RHOGVX], g_TEND_pl[:,:,:,I_RHOGVX], # [OUT]   # pl 2,0  sign reversed
+                        g_TEND[:,:,:,:,I_RHOGVY], g_TEND_pl[:,:,:,I_RHOGVY], # [OUT]   # pl 2,0  sign of #5 reversed
+                        g_TEND[:,:,:,:,I_RHOGVZ], g_TEND_pl[:,:,:,I_RHOGVZ], # [OUT]   # pl 2,0  sign of #5 reversed, others off
+                        g_TEND[:,:,:,:,I_RHOGW],  g_TEND_pl[:,:,:,I_RHOGW],  # [OUT]   # pl 2,0  sign of #5 reversed, others off
                         rcnf, cnst, grd, oprt, vmtr, rdtype,
                 )
 
@@ -727,6 +771,29 @@ class Dyn:
                 # Zero out specific components of g_TEND_pl
                 g_TEND_pl[:, :, :, I_RHOG]  = 0.0
                 g_TEND_pl[:, :, :, I_RHOGE] = 0.0
+
+
+                with open(std.fname_log, 'a') as log_file:
+                    print("DIAG_pl[0, 2, 0, :]", DIAG_pl[0, 2, 0, :], file=log_file)
+                    print("DIAG_pl[1, 2, 0, :]", DIAG_pl[1, 2, 0, :], file=log_file)
+                    print("DIAG_pl[2, 2, 0, :]", DIAG_pl[2, 2, 0, :], file=log_file)
+                    print("DIAG_pl[3, 2, 0, :]", DIAG_pl[3, 2, 0, :], file=log_file)
+                    print("DIAG_pl[4, 2, 0, :]", DIAG_pl[4, 2, 0, :], file=log_file)
+                    print("DIAG_pl[5, 2, 0, :]", DIAG_pl[5, 2, 0, :], file=log_file)
+
+                    print("PROG_pl[0, 2, 0, :]", PROG_pl[0, 2, 0, :], file=log_file)
+                    print("PROG_pl[1, 2, 0, :]", PROG_pl[1, 2, 0, :], file=log_file)
+                    print("PROG_pl[2, 2, 0, :]", PROG_pl[2, 2, 0, :], file=log_file)
+                    print("PROG_pl[3, 2, 0, :]", PROG_pl[3, 2, 0, :], file=log_file)
+                    print("PROG_pl[4, 2, 0, :]", PROG_pl[4, 2, 0, :], file=log_file)
+                    print("PROG_pl[5, 2, 0, :]", PROG_pl[5, 2, 0, :], file=log_file)    # DAIG_pl and PROG_pl ALL good at 2,0
+                    #print("g_TEND     (0,2,0,:)", g_TEND   [0, 2, 0, :], file=log_file)       
+                    print("g_TEND_pl  (0,2,0,:)", g_TEND_pl[0, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl  (1,2,0,:)", g_TEND_pl[1, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl  (2,2,0,:)", g_TEND_pl[2, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl  (3,2,0,:)", g_TEND_pl[3, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl  (4,2,0,:)", g_TEND_pl[4, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl  (5,2,0,:)", g_TEND_pl[5, 2, 0, :], file=log_file)  # sign reversed
 
                 #---< numerical diffusion term
                 if rcnf.NDIFF_LOCATION == 'IN_LARGE_STEP':
@@ -823,16 +890,25 @@ class Dyn:
                 #   call NDG_apply_uvtp
                 #   endif
 
+                with open(std.fname_log, 'a') as log_file:  
+                    print("g_TEND_pl added (0,2,0,:)", g_TEND_pl[0, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl added (1,2,0,:)", g_TEND_pl[1, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl added (2,2,0,:)", g_TEND_pl[2, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl added (3,2,0,:)", g_TEND_pl[3, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl added (4,2,0,:)", g_TEND_pl[4, 2, 0, :], file=log_file) 
+                    print("g_TEND_pl added (5,2,0,:)", g_TEND_pl[5, 2, 0, :], file=log_file) 
+
+                    print("f_TEND_pl added (0,2,0,:)", f_TEND_pl[0, 2, 0, :], file=log_file) 
+                    print("f_TEND_pl added (1,2,0,:)", f_TEND_pl[1, 2, 0, :], file=log_file) 
+                    print("f_TEND_pl added (2,2,0,:)", f_TEND_pl[2, 2, 0, :], file=log_file) 
+                    print("f_TEND_pl added (3,2,0,:)", f_TEND_pl[3, 2, 0, :], file=log_file) 
+                    print("f_TEND_pl added (4,2,0,:)", f_TEND_pl[4, 2, 0, :], file=log_file) 
+                    print("f_TEND_pl added (5,2,0,:)", f_TEND_pl[5, 2, 0, :], file=log_file) 
+
+
                 g_TEND[:, :, :, :, 0:6] += f_TEND[:, :, :, :, 0:6]
 
                 g_TEND_pl += f_TEND_pl
-
-
-                # with open(std.fname_log, 'a') as log_file:  
-                #     print("g_TEND added (6,5,2,0,:)", g_TEND[6, 5, 2, 0, :], file=log_file) 
-                #     print("g_TEND added (5,6,2,0,:)", g_TEND[5, 6, 2, 0, :], file=log_file) 
-
-
 
                 prf.PROF_rapend('__Large_step',1)
                 #------------------------------------------------------------------------

pynicamdc/nhm/dynamics/mod_src.py
@@ -63,7 +63,7 @@ class Src:
                 rhogvy,  rhogvy_pl,    # [IN]
                 rhogvz,  rhogvz_pl,    # [IN]
                 rhogw,   rhogw_pl,     # [IN]
-                grhogvx, grhogvx_pl,   # [OUT] 
+                grhogvx, grhogvx_pl,   # [OUT]   grhogvx very different for vindex > 0
                 grhogvy, grhogvy_pl,   # [OUT] 
                 grhogvz, grhogvz_pl,   # [OUT] 
                 grhogw,  grhogw_pl,    # [OUT] 
@@ -102,6 +102,7 @@ class Src:
             self.vvy[:, :, kmin:kmaxp1, :] = vy[:, :, kmin:kmaxp1, :]
 
             # Prepare GRD factors (shape: (kmaxp1 - kmin, 1, 1, 1))
+            # ? (shape: (1, 1, kmaxp1 - kmin, 1)) seems correct
             cfact = grd.GRD_cfact[kmin:kmaxp1][None, None, :, None]
             dfact = grd.GRD_dfact[kmin:kmaxp1][None, None, :, None]
 
@@ -126,17 +127,17 @@ class Src:
 
 
             # Reshape cfact/dfact to broadcast over (i, j, l)
-            cfact = grd.GRD_cfact[kmin:kmaxp1][None, None, :, None]  # shape (k, 1, 1, 1)
+            cfact = grd.GRD_cfact[kmin:kmaxp1][None, None, :, None]  # shape (k, 1, 1, 1) ? (1,1,k,1) seems correct
             dfact = grd.GRD_dfact[kmin:kmaxp1][None, None, :, None]
 
             # wc = GRD_cfact * w[k+1] + GRD_dfact * w[k]
             wc = (
                 cfact * w[:, :, kminp1:kmaxp2, :] +
-                dfact * w[:, :, kmin:kmaxp1,     :]
+                dfact * w[:, :, kmin:kmaxp1,   :]
             )  # shape: (i, j, k, l)
 
             # Prepare GRD_x directional components
-            gx = grd.GRD_x[:, :, 0, :, XDIR].copy()  # shape: (i, j, l)
+            gx = grd.GRD_x[:, :, 0, :, XDIR].copy()  # shape: (i, j, l)  
             gy = grd.GRD_x[:, :, 0, :, YDIR].copy()
             gz = grd.GRD_x[:, :, 0, :, ZDIR].copy()
 
@@ -166,7 +167,7 @@ class Src:
             wc = np.empty((adm.ADM_gall_pl, kmaxp1 - kmin, adm.ADM_lall_pl), dtype=w_pl.dtype)
 
             # GRD_cfact and GRD_dfact reshaped for broadcasting
-            cfact = grd.GRD_cfact[kmin:kmaxp1][None, :, None]  # shape: (k, 1, 1)
+            cfact = grd.GRD_cfact[kmin:kmaxp1][None, :, None]  # shape: (k, 1, 1)? (1,k,1) seems correct
             dfact = grd.GRD_dfact[kmin:kmaxp1][None, :, None]
 
             # Compute wc = GRD_cfact * w[k+1] + GRD_dfact * w[k]
@@ -201,6 +202,12 @@ class Src:
             self.vvy_pl[:, kmaxp1, :] = 0.0
             self.vvz_pl[:, kminm1, :] = 0.0
             self.vvz_pl[:, kmaxp1, :] = 0.0
+
+            with open(std.fname_log, 'a') as log_file:
+                print("vvxyz_pl check before calculating dvvxyz_pl", file=log_file)
+                print("self.vvx_pl (:,2,0)", self.vvx_pl [:, 2, 0], file=log_file) 
+                print("self.vvy_pl (:,2,0)", self.vvy_pl [:, 2, 0], file=log_file) 
+                print("self.vvz_pl (:,2,0)", self.vvz_pl [:, 2, 0], file=log_file)   # all good at 2,0
 
         #endif
 
@@ -243,19 +250,20 @@ class Src:
         )
 
  
-        # with open(std.fname_log, 'a') as log_file:  
+        with open(std.fname_log, 'a') as log_file:  
+            kc=2
         #     print("self.vvx (6,5,2,0)", self.vvx [6, 5, 2, 0], file=log_file) 
         #     print("self.vvy (6,5,2,0)", self.vvy [6, 5, 2, 0], file=log_file) 
         #     print("self.vvz (6,5,2,0)", self.vvz [6, 5, 2, 0], file=log_file) 
         #     print("self.dvvx(6,5,2,0)", self.dvvx[6, 5, 2, 0], file=log_file) 
         #     print("self.dvvy(6,5,2,0)", self.dvvy[6, 5, 2, 0], file=log_file) 
-        #     print("self.dvvz(6,5,2,0)", self.dvvz[6, 5, 2, 0], file=log_file)
-        #     print("self.vvx_pl (0,21,0)", self.vvx_pl [0, 20, 0], file=log_file) 
-        #     print("self.vvy_pl (0,21,0)", self.vvy_pl [0, 20, 0], file=log_file) 
-        #     print("self.vvz_pl (0,21,0)", self.vvz_pl [0, 20, 0], file=log_file) 
-        #     print("self.dvvx_pl(0,21,0)", self.dvvx_pl[0, 20, 0], file=log_file) 
-        #     print("self.dvvy_pl(0,21,0)", self.dvvy_pl[0, 20, 0], file=log_file) 
-        #     print("self.dvvz_pl(0,21,0)", self.dvvz_pl[0, 20, 0], file=log_file) 
+        #    print("self.dvvz(6,5,2,0)", self.dvvz[6, 5, 2, 0], file=log_file)
+            print(f"self.vvx_pl (:,{kc},0)", self.vvx_pl [:, kc, 0], file=log_file) 
+            print(f"self.vvy_pl (:,{kc},0)", self.vvy_pl [:, kc, 0], file=log_file) 
+            print(f"self.vvz_pl (:,{kc},0)", self.vvz_pl [:, kc, 0], file=log_file) 
+            print(f"self.dvvx_pl(:,{kc},0)", self.dvvx_pl[:, kc, 0], file=log_file)  # differs from original, but perhaps because the numbers are very small
+            print(f"self.dvvy_pl(:,{kc},0)", self.dvvy_pl[:, kc, 0], file=log_file) 
+            print(f"self.dvvz_pl(:,{kc},0)", self.dvvz_pl[:, kc, 0], file=log_file) 
 
         if grd.GRD_grid_type == grd.GRD_grid_type_on_plane:
 
@@ -351,7 +359,7 @@ class Src:
 #alpha = NON_HYDRO_ALPHA  # real scalar
 
             # --- Coriolis force ---
-            self.dvvx_pl[:, kmin:kmaxp1, :] -= -2.0 * rhog_pl[:, kmin:kmaxp1, :] * (-ohm * self.vvy_pl[:, kmin:kmaxp1, :])
+            self.dvvx_pl[:, kmin:kmaxp1, :] -= -2.0 * rhog_pl[:, kmin:kmaxp1, :] * ( ohm * self.vvy_pl[:, kmin:kmaxp1, :])
             self.dvvy_pl[:, kmin:kmaxp1, :] -=  2.0 * rhog_pl[:, kmin:kmaxp1, :] * ( ohm * self.vvx_pl[:, kmin:kmaxp1, :])
 
             # --- Horizontalize and separate vertical velocity ---
@@ -444,7 +452,7 @@ class Src:
         if fluxtype == self.I_SRC_default:
 
             # Pre-broadcasted afact and bfact for performance
-            afact = grd.GRD_afact[kmin:kmaxp2][None, None, :, None]  # shape (k, 1, 1, 1)
+            afact = grd.GRD_afact[kmin:kmaxp2][None, None, :, None]  # shape (k, 1, 1, 1)?  seems like (1,1,k,1) is correct
             bfact = grd.GRD_bfact[kmin:kmaxp2][None, None, :, None]
 
             # Allocate or reuse a temporary array for weighted scalar field
@@ -463,7 +471,7 @@ class Src:
 
             if adm.ADM_have_pl:
 
-                afact = grd.GRD_afact[kmin:kmaxp2][None, :, None]  # (k, 1, 1)
+                afact = grd.GRD_afact[kmin:kmaxp2][None, :, None]  # (k, 1, 1) ? seems like (1,k,1) is correct
                 bfact = grd.GRD_bfact[kmin:kmaxp2][None, :, None]
 
                 weighted_scl_pl = (
@@ -484,16 +492,17 @@ class Src:
 
         #endif
 
-        # with open(std.fname_log, 'a') as log_file:  
-        #     print("before flux convergence", file=log_file)
+        with open(std.fname_log, 'a') as log_file:
+            kc=2
+            print("before flux convergence", file=log_file)
         #     print("self.rhogvxscl (6,5,2,0)", self.rhogvxscl[6, 5, 2, 0], file=log_file) 
         #     print("self.rhogvyscl (6,5,2,0)", self.rhogvyscl[6, 5, 2, 0], file=log_file) 
         #     print("self.rhogvzscl (6,5,2,0)", self.rhogvzscl[6, 5, 2, 0], file=log_file) 
         #     print("self.rhogwscl (6,5,2,0)", self.rhogwscl[6, 5, 2, 0], file=log_file)
-        #     print("self.rhogvxscl_pl (0,20,0)", self.rhogvxscl_pl[0, 20, 0], file=log_file)
-        #     print("self.rhogvyscl_pl (0,20,0)", self.rhogvyscl_pl[0, 20, 0], file=log_file)
-        #     print("self.rhogvzscl_pl (0,20,0)", self.rhogvzscl_pl[0, 20, 0], file=log_file)
-        #     print("self.rhogwscl_pl (0,20,0)", self.rhogwscl_pl[0, 20, 0], file=log_file)
+            print(f"self.rhogvxscl_pl (:,{kc},0)", self.rhogvxscl_pl[:, kc, 0], file=log_file)
+            print(f"self.rhogvyscl_pl (:,{kc},0)", self.rhogvyscl_pl[:, kc, 0], file=log_file)
+            print(f"self.rhogvzscl_pl (:,{kc},0)", self.rhogvzscl_pl[:, kc, 0], file=log_file)
+            print(f"self.rhogwscl_pl  (:,{kc},0)", self.rhogwscl_pl [:, kc, 0], file=log_file)
         
         #--- flux convergence step
 
@@ -507,10 +516,10 @@ class Src:
                 grd, oprt, vmtr, rdtype, 
         )
 
-        # with open(std.fname_log, 'a') as log_file:
-        #     print("after flux convergence", file=log_file)
-        #     print("grhogscl (6,5,2,0)", grhogscl[6, 5, 2, 0], file=log_file)
-        #     print("grhogscl_pl (0,20,0)", grhogscl_pl[0, 20, 0], file=log_file)
+        with open(std.fname_log, 'a') as log_file:
+            print("after flux convergence", file=log_file)
+            print("grhogscl (6,5,2,0)", grhogscl[6, 5, 2, 0], file=log_file)
+            print("grhogscl_pl (0,20,0)", grhogscl_pl[0, 20, 0], file=log_file)
 
         prf.PROF_rapend('____src_advection_conv',2)
 
@@ -666,11 +675,11 @@ class Src:
         #     print("self.rhogvx_vm(6,5,2,0)", self.rhogvx_vm[6, 5, 2, 0], file=log_file) 
         #     print("self.rhogvy_vm(6,5,2,0)", self.rhogvy_vm[6, 5, 2, 0], file=log_file) 
         #     print("self.rhogvz_vm(6,5,2,0)", self.rhogvz_vm[6, 5, 2, 0], file=log_file) 
-        #     #print("oprt.OPRT_coef_div(6,5,:,2,0)", oprt.OPRT_coef_div[6, 5, :, 2, 0], file=log_file) 
-        #     print("div_rhogvh_pl(0,20,0)", div_rhogvh_pl[0,20,0], file=log_file) 
-        #     print("self.rhogvx_vm_pl(0,20,0)", self.rhogvx_vm_pl[0,20,0], file=log_file) 
-        #     print("self.rhogvy_vm_pl(0,20,0)", self.rhogvy_vm_pl[0,20,0], file=log_file) 
-        #     print("self.rhogvz_vm_pl(0,20,0)", self.rhogvz_vm_pl[0,20,0], file=log_file) 
+            #print("oprt.OPRT_coef_div(6,5,:,2,0)", oprt.OPRT_coef_div[6, 5, :, 2, 0], file=log_file) 
+            # print("div_rhogvh_pl(0,20,0)", div_rhogvh_pl[0,20,0], file=log_file) 
+            # print("self.rhogvx_vm_pl(0,20,0)", self.rhogvx_vm_pl[0,20,0], file=log_file) 
+            # print("self.rhogvy_vm_pl(0,20,0)", self.rhogvy_vm_pl[0,20,0], file=log_file) 
+            # print("self.rhogvz_vm_pl(0,20,0)", self.rhogvz_vm_pl[0,20,0], file=log_file) 
             #print("oprt.OPRT_coef_div_pl(:,20,0)", oprt.OPRT_coef_div_pl[:, 20, 0], file=log_file) 
             #print("horiz(6,5,2)", horiz[6, 5, 2], file=log_file) 
             #print("flux_diff(6,5,2,0)", flux_diff[6, 5, 2, 0], file=log_file)       
@@ -686,10 +695,14 @@ class Src:
             grd, rdtype,
         ) 
 
-        # with open(std.fname_log, 'a') as log_file:  
-        #     print("out from OPRT_divergence", file=log_file)
-        #     print("div_rhogvh(6,5,2,0)", div_rhogvh[6, 5, 2, 0], file=log_file) 
-        #     print("div_rhogvh_pl(0,20,0)", div_rhogvh_pl[0,20,0], file=log_file) 
+        with open(std.fname_log, 'a') as log_file:  
+            kc=2
+            print("AFTEROPRT_divergence", file=log_file)
+            print(f"div_rhogvh(6,5,{kc},0)", div_rhogvh[6, 5, kc, 0], file=log_file) 
+            print(f"div_rhogvh_pl(:,{kc},0)", div_rhogvh_pl[:,kc,0], file=log_file) 
+            print(f"self.rhogvx_vm_pl(:,{kc},0)", self.rhogvx_vm_pl[:,kc,0], file=log_file)
+            print(f"self.rhogvy_vm_pl(:,{kc},0)", self.rhogvy_vm_pl[:,kc,0], file=log_file)
+            print(f"self.rhogvz_vm_pl(:,{kc},0)", self.rhogvz_vm_pl[:,kc,0], file=log_file)
 
         #--- Total flux convergence
 
@@ -731,10 +744,10 @@ class Src:
             flux_diff_pl = self.rhogw_vmh_pl[:, kminp1:kmaxp2, :] - self.rhogw_vmh_pl[:, kmin:kmaxp1, :]  # shape: (g, k, l)
 
             # GRD_rdgz[k] needs reshaping for broadcasting: (k, 1)
-            rdgz = grd.GRD_rdgz[kmin:kmaxp1][:, None]  # (k, 1)
+            rdgz = grd.GRD_rdgz[kmin:kmaxp1][None, :, None]  # (1, k, 1)
 
             # Apply vertical term
-            vert_term_pl = flux_diff_pl * rdgz[None, :, :]  # shape: (g, k, l)
+            vert_term_pl = flux_diff_pl * rdgz   #[None, :, :]  # shape: (g, k, l)
 
             # Final grhog update
             grhog_pl[:, kmin:kmaxp1, :] = - div_rhogvh_pl[:, kmin:kmaxp1, :] - vert_term_pl
@@ -742,6 +755,21 @@ class Src:
             # Set ghost layers to zero
             grhog_pl[:, kminm1, :] = 0.0
             grhog_pl[:, kmaxp1, :] = 0.0
+
+
+            with open(std.fname_log, 'a') as log_file:  
+                kc=2 - 1
+                print("ABOUTtoFinish_flux_convergence", file=log_file)
+                #print(f"div_rhogvh(6,5,{kc},0)", div_rhogvh[6, 5, kc, 0], file=log_file) 
+                print(f"grhog_pl(:,{kc},0)", grhog_pl[:,kc,0], file=log_file) 
+                print(f"div_rhogvh_pl(:,{kc},0)", div_rhogvh_pl[:,kc,0], file=log_file)
+                print(f"flux_diff_pl(:,{kc},0)", flux_diff_pl[:,kc,0], file=log_file)
+                print(f"rdgz(:,{kc},0)", rdgz[:,kc,0], file=log_file)
+                print(f"self.rhogw_vmh_pl(:,{kc+1},0)", self.rhogw_vmh_pl[:,kc+1,0], file=log_file)
+                print(f"self.rhogw_vmh_pl(:,{kc},0)", self.rhogw_vmh_pl[:,kc,0], file=log_file)
+                #print(f"self.rhogw_vmh_pl(:,{kc},0)", self.rhogw_vmh_pl[:,kc,0], file=log_file)
+                #print(f"vert_term_pl(:,{kc},0)", vert_term_pl[:,kc,0], file=log_file)
+                #print(f"self.rhogvz_vm_pl(:,{kc},0)", self.rhogvz_vm_pl[:,kc,0], file=log_file)
 
 
         prf.PROF_rapend('____src_flux_conv',2)

pynicamdc/nhm/share/mod_bndcnd.py
@@ -258,6 +258,133 @@ class Bndc:
 
         return
     
+
+    def BNDCND_all_pl(
+        self,
+        idim, 
+        kdim, 
+        ldim, 
+        rho,       # (idim, kdim, ldim)  density
+        vx,        # (idim, kdim, ldim)  horizontal wind (x)
+        vy,        # (idim, kdim, ldim)  horizontal wind (y) 
+        vz,        # (idim, kdim, ldim)  horizontal wind (z)
+        w,         # (idim, kdim, ldim)  vertical wind           ####
+        ein,       # (idim, kdim, ldim)  internal energy
+        tem,       # (idim, kdim, ldim)  temperature
+        pre,       # (idim, kdim, ldim)  pressure
+        rhog,
+        rhogvx,
+        rhogvy,
+        rhogvz,
+        rhogw,                                                         ####
+        rhoge,
+        gsqrtgam2,  
+        phi,       # (idim, kdim, ldim)  geopotential
+        c2wfact,    
+        c2wfact_Gz,
+        cnst,
+        rdtype,
+    ):
+
+        kmin = adm.ADM_kmin
+        kmax = adm.ADM_kmax
+        kmaxp1 = kmax + 1
+        kminm1 = kmin - 1
+        CVdry = cnst.CONST_CVdry
+
+
+        # with open(std.fname_log, 'a') as log_file:
+        #     print("ZERO0", file=log_file)
+        #     print(tem[16,0,kmaxp1,0], file=log_file)
+        #     print(rho[16,0,kmaxp1,0], gsqrtgam2[16,0,kmaxp1,0], file=log_file)
+        #     print(pre[16,0,kmaxp1,0], file=log_file)
+        #     print(phi[16,0,kmaxp1,0], file=log_file)
+        #     print(phi[16,0,kmax,0], file=log_file)    
+            #print(phi[16,0,3,0], file=log_file)    
+            #print(phi[16,0,0,0], file=log_file)    
+            #print(phi[10,10,3,0], file=log_file)    
+            #print(rho[16,0,kmax,0],gsqrtgam2[16,0,kmax,0], file=log_file)
+            #print(rho[17,0,kmaxp1,0],gsqrtgam2[17,0,kmaxp1,0], file=log_file)   
+            #print(rho[17,0,kmax,0],gsqrtgam2[17,0,kmax,0], file=log_file)
+
+        #--- Thermodynamical variables ( rho, ein, tem, pre, rhog, rhoge ), q = 0 at boundary
+        self.BNDCND_thermo_pl(
+            tem, rho, pre, phi, 
+            cnst, rdtype
+        )
+
+        rhog[:, kmaxp1, :] = rho[:, kmaxp1, :] * gsqrtgam2[:, kmaxp1, :]
+        rhog[:, kminm1, :] = rho[:, kminm1, :] * gsqrtgam2[:, kminm1, :]
+        ein[:, kmaxp1, :] = CVdry * tem[:, kmaxp1, :]
+        ein[:, kminm1, :] = CVdry * tem[:, kminm1, :]
+        rhoge[:, kmaxp1, :] = rhog[:, kmaxp1, :] * ein[:, kmaxp1, :]
+        rhoge[:, kminm1, :] = rhog[:, kminm1, :] * ein[:, kminm1, :]
+
+        # with open(std.fname_log, 'a') as log_file:
+        #     print("ZERO1", file=log_file)
+        #     print(rho[16,0,kmaxp1,0],gsqrtgam2[16,0,kmaxp1,0], file=log_file)
+        #     print(rho[16,0,kmax,0],gsqrtgam2[16,0,kmax,0], file=log_file)
+        #     print(rho[17,0,kmaxp1,0],gsqrtgam2[17,0,kmaxp1,0], file=log_file)   
+        #     print(rho[17,0,kmax,0],gsqrtgam2[17,0,kmax,0], file=log_file)
+#            print(c2wfact[17,0,kmaxp1,0,0],c2wfact[17,0,kmaxp1,1,0], rhog[17,0,kmaxp1,0],rhog[17,0,kmax,0], file=log_file)  
+#            print(c2wfact[16,1,kmaxp1,0,0],c2wfact[16,1,kmaxp1,1,0], rhog[16,1,kmaxp1,0],rhog[16,1,kmax,0], file=log_file)
+#            print(c2wfact[17,1,kmaxp1,0,0],c2wfact[17,1,kmaxp1,1,0], rhog[17,1,kmaxp1,0],rhog[17,1,kmax,0], file=log_file)  
+#            print(c2wfact[10,10,kmaxp1,0,0],c2wfact[10,10,kmaxp1,1,0], rhog[10,10,kmaxp1,0],rhog[10,10,kmax,0], file=log_file)  
+
+
+        #--- Momentum ( rhogvx, rhogvy, rhogvz, vx, vy, vz )
+        self.BNDCND_rhovxvyvz_pl(
+            rhog, rhogvx, rhogvy, rhogvz
+        )
+        
+
+        vx[:, kmaxp1, :] = rhogvx[:, kmaxp1, :] / rhog[:, kmaxp1, :]
+        vx[:, kminm1, :] = rhogvx[:, kminm1, :] / rhog[:, kminm1, :]
+        vy[:, kmaxp1, :] = rhogvy[:, kmaxp1, :] / rhog[:, kmaxp1, :]
+        vy[:, kminm1, :] = rhogvy[:, kminm1, :] / rhog[:, kminm1, :]
+        vz[:, kmaxp1, :] = rhogvz[:, kmaxp1, :] / rhog[:, kmaxp1, :]
+        vz[:, kminm1, :] = rhogvz[:, kminm1, :] / rhog[:, kminm1, :]
+
+
+        #--- Momentum ( rhogw, w )
+        self.BNDCND_rhow_pl(
+            rhogvx, rhogvy, rhogvz, rhogw, c2wfact_Gz
+        )
+
+        # with open(std.fname_log, 'a') as log_file:
+        #     print("ZEROc2w", file=log_file)
+        #     print(c2wfact[16,0,kmaxp1,0,0],c2wfact[16,0,kmaxp1,1,0], rhog[16,0,kmaxp1,0],rhog[16,0,kmax,0], file=log_file)
+        #     print(c2wfact[17,0,kmaxp1,0,0],c2wfact[17,0,kmaxp1,1,0], rhog[17,0,kmaxp1,0],rhog[17,0,kmax,0], file=log_file)  
+        #     print(c2wfact[16,1,kmaxp1,0,0],c2wfact[16,1,kmaxp1,1,0], rhog[16,1,kmaxp1,0],rhog[16,1,kmax,0], file=log_file)
+        #     print(c2wfact[17,1,kmaxp1,0,0],c2wfact[17,1,kmaxp1,1,0], rhog[17,1,kmaxp1,0],rhog[17,1,kmax,0], file=log_file)  
+        #     print(c2wfact[10,10,kmaxp1,0,0],c2wfact[10,10,kmaxp1,1,0], rhog[10,10,kmaxp1,0],rhog[10,10,kmax,0], file=log_file)  
+
+        # for i in range(idim):
+        #     for j in range(jdim):
+        #         for l in range(ldim):
+        #             if c2wfact[i, j, kmaxp1, 0, l] * rhog[i, j, kmaxp1, l] + c2wfact[i, j, kmaxp1, 1, l] * rhog[i, j, kmax, l] ==0.0 :
+        #                 print("i, j, kmaxp1, kmax, l", i, j, kmaxp1, kmax, l)
+        #                 print(c2wfact[i,j,kmaxp1, 0, l], c2wfact[i, j, kmaxp1, 1, l])
+#                              , rhog[i, j, kmaxp1, l], c2wfact[i, j, kmaxp1, 1, l], rhog[i, j, kmax, l]) 
+
+        # print("stopping")
+        # prc.prc_mpistop(std.io_l, std.fname_log)
+
+        w[:, kmaxp1, :] = rhogw[:, kmaxp1, :] / (
+            c2wfact[:, kmaxp1, 0, :] * rhog[:, kmaxp1, :] +
+            c2wfact[:, kmaxp1, 1, :] * rhog[:, kmax, :]
+        )
+
+        w[:, kmin, :] = rhogw[:, kmin, :] / (
+            c2wfact[:, kmin, 0, :] * rhog[:, kmin,   :] +
+            c2wfact[:, kmin, 1, :] * rhog[:, kminm1, :]
+        )
+
+        w[:, kminm1, :] = 0.0
+
+        return
+    
+
     def BNDCND_thermo(
         self,
         tem, rho, pre, phi, 
@@ -342,6 +469,92 @@ class Bndc:
 
         return
     
+
+    def BNDCND_thermo_pl(
+        self,
+        tem, rho, pre, phi, 
+        cnst, rdtype
+    ):
+
+        kmin = adm.ADM_kmin
+        kmax = adm.ADM_kmax
+        kminm1   = kmin - 1
+        kminp1   = kmin + 1
+        kminp2   = kmin + 2
+        kmaxm1   = kmax - 1
+        kmaxm2   = kmax - 2
+        kmaxp1   = kmax + 1
+        GRAV = cnst.CONST_GRAV
+        Rdry = cnst.CONST_Rdry
+
+
+        # Vectorized Lagrange interpolation
+        def lag_intpl_vec(z, z1, p1, z2, p2, z3, p3):
+            return (
+                ((z - z2) * (z - z3)) / ((z1 - z2) * (z1 - z3)) * p1 +
+                ((z - z1) * (z - z3)) / ((z2 - z1) * (z2 - z3)) * p2 +
+                ((z - z1) * (z - z2)) / ((z3 - z1) * (z3 - z2)) * p3
+            )
+
+        # -----------------------
+        # Top temperature boundary
+        # -----------------------
+        if self.is_top_tem:
+            tem[:, kmaxp1, :] = tem[:, kmax, :]
+
+        elif self.is_top_epl:
+            z  = phi[:, kmaxp1, :] / GRAV
+            z1 = phi[:, kmax,   :] / GRAV
+            z2 = phi[:, kmaxm1, :] / GRAV
+            z3 = phi[:, kmaxm2, :] / GRAV
+
+            tem[:, kmaxp1, :] = lag_intpl_vec(
+                z,
+                z1, tem[:, kmax,   :],
+                z2, tem[:, kmaxm1, :],
+                z3, tem[:, kmaxm2, :]
+            )
+
+        # -----------------------
+        # Bottom temperature boundary
+        # -----------------------
+        if self.is_btm_tem:
+            tem[:, kminm1, :] = tem[:, kmin, :]
+
+        elif self.is_btm_epl:
+            z1 = phi[:, kminp2, :] / GRAV
+            z2 = phi[:, kminp1, :] / GRAV
+            z3 = phi[:, kmin,   :] / GRAV
+            z  = phi[:, kminm1, :] / GRAV
+
+            tem[:, kminm1, :] = lag_intpl_vec(
+                z,
+                z1, tem[:, kminp2, :],
+                z2, tem[:, kminp1, :],
+                z3, tem[:, kmin,   :]
+            )
+
+        # -----------------------
+        # Pressure boundary (hydrostatic)
+        # -----------------------
+        pre[:, kmaxp1, :] = pre[:, kmaxm1, :] - rho[:, kmax, :] * (
+            phi[:, kmaxp1, :] - phi[:, kmaxm1, :]
+        )
+
+
+        pre[:, kminm1, :] = pre[:, kminp1, :] - rho[:, kmin, :] * (
+            phi[:, kminm1, :] - phi[:, kminp1, :]
+        )
+
+        # -----------------------
+        # Density boundary (equation of state)
+        # -----------------------
+        rho[:, kmaxp1, :] = pre[:, kmaxp1, :] / (Rdry * tem[:, kmaxp1, :])
+        rho[:, kminm1, :] = pre[:, kminm1, :] / (Rdry * tem[:, kminm1, :])
+
+        return
+    
+    
     def BNDCND_rhovxvyvz(
         self,
         rhog, rhogvx, rhogvy, rhogvz
@@ -353,7 +566,7 @@ class Bndc:
         kmaxp1   = kmax + 1
 
        # Allocate reusable buffer once inside the function
-        scale = np.empty_like(rhog[:, :, 0, :])  # shape = (idim, jdim, ldim)
+        scale = np.empty_like(rhog[:, :, 0, :])  # shape = (idim, ldim)
 
         # --- Top boundary (k = kmax + 1) ---
         if self.is_top_rigid:
@@ -410,7 +623,7 @@ class Bndc:
         kmaxp1   = kmax + 1
 
        # Allocate reusable buffer once inside the function
-        scale = np.empty_like(rhog[:, 0, :])  # shape = (idim, jdim, ldim)
+        scale = np.empty_like(rhog[:, 0, :])  # shape = (idim, ldim)
 
         # --- Top boundary (k = kmax + 1) ---
         if self.is_top_rigid:

pynicamdc/share/mod_oprt.py
@@ -1368,6 +1368,9 @@ class Oprt:
         #scl = np.zeros((adm.ADM_gall_1d, adm.ADM_gall_1d, adm.ADM_kdall, adm.ADM_lall), dtype=rdtype)
         #scl_pl = np.zeros((adm.ADM_gall_pl, adm.ADM_kdall, adm.ADM_lall_pl), dtype=rdtype)
 
+        scl[:, :, :, :] = 0.0
+        #scl_pl[:, :, :] = 0.0
+
         #gall   = adm.ADM_gall
         iall  = adm.ADM_gall_1d
         jall  = adm.ADM_gall_1d
@@ -1471,8 +1474,8 @@ class Oprt:
                     #         print(scl_pl[n, 20, 0], file=log_file)
 
                         #  v-1 for coef and v for vx_pl in f, but should be v and v in p (0 - 5)
-        #else:
-        #    scl_pl[:, :, :] = 0.0
+        else:
+            scl_pl[:, :, :] = 0.0
 
         # with open(std.fname_log, 'a') as log_file:
         #     print("out: scl", file=log_file)